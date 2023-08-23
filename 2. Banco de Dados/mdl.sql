3a30dfefb3f2f19d3cccf0cd7eaf00d55958397e
Luiz-Dias01 <142491324+Luiz-Dias01@users.noreply.github.com>
2023-08-23 19:55:56 -0300

    Update mdl.sql

--- 2. Banco de Dados/mdl.sql
+++ 2. Banco de Dados/mdl.sql
@@ -1,34 +1,34 @@
 -- Inserir Registro
 INSERT INTO tb_agenda (nm_contato, ds_telefone, ds_email, bt_favorito, dt_cadastro)
-	VALUES ('Ketlen', '+351 927-738-530', 'kehportuga@gmail.com', true, '2023-08-16 11:07:50');
+	VALUES ('?', '?', '?', true, '?');
 
 -- Buscar por Todos     
 SELECT * FROM tb_agenda;
 
 -- Buscar por nome
 SELECT nm_contato FROM tb_agenda
-	WHERE nm_contato = 'Maria alice';
+	WHERE nm_contato = '?';
 
 
 -- Buscar por Favorito
 SELECT bt_favorito FROM tb_agenda
-	WHERE bt_favorito = true;
+	WHERE bt_favorito = ?;
 
 
 -- Buscar por Intervalo  
 SELECT dt_cadastro FROM tb_agenda
-	WHERE dt_cadastro BETWEEN '2023-08-01 00:00:00' AND '2023-08-30 00:00:00';
+	WHERE dt_cadastro BETWEEN '?' AND '?';
 
 
 -- ALterar Registro    
 UPDATE tb_agenda 
-	SET nm_contato = 'Sophia',
-		ds_telefone = '+55 (11) 97595-8925',
-        ds_email = 'sophiasoh@gmail.com',
+	SET nm_contato = '?',
+		ds_telefone = '?',
+        ds_email = '?',
         bt_favorito = false,
-        dt_cadastro = '2023-08-19 11:24:25'
-		WHERE id_agenda = 2;
+        dt_cadastro = '?'
+		WHERE id_agenda = ?;
 
 -- Apagar Registro       
 DELETE FROM tb_agenda
-	WHERE id_agenda = 2;
+	WHERE id_agenda = ?;
